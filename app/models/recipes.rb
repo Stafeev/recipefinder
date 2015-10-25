35636306e1f5c9cbee162e14d086a0fb0a7791e5
Stafeev <dunlop300g@yandex.ru>
2015-10-25 15:35:18 -0700

    first commit

---
+++ app/models/recipes.rb
@@ -0,0 +1,18 @@
+require 'httparty'
+class Recipes
+	include HTTParty
+
+	#key_value = ENV[’FOOD2FORK_KEY’]
+	key_value = "f92d5ad557fa20e51e16df3a7f8d0b35"
+	#hostport = ENV[’FOOD2FORK_SERVER_AND_PORT’] || ’www.food2fork.com’ 
+	hostport = "www.food2fork.com"
+	base_uri "http://#{hostport}/api"
+
+    
+	default_params key: key_value
+	format :json
+
+	def self.for term
+		get("/search", query: {q: term})["recipes"]
+	end
+end
